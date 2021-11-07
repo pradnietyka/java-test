c525e043d87d4329bb779e74c74a47a2a17e90fa
Michał <michal.lukaszewski3@gmail.com>
2021-11-07 14:47:39 +0100

    Czwarty Commit

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,48 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-       int liczba = 10;
-       int liczba2 = 10;
+////       int liczba = 10;
+////       int liczba2 = 10;
+////
+////       System.out.println("Hello word" + liczba);
+////
+////       System.out.println(liczba % liczba2);
+////
+////        String name = "Michał";
+////        int age = 28;
+////        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
+////
+////        String account = " MartaKubala ";
+////        System.out.println(account.length());
+////        System.out.println(account.indexOf('r'));
+////        System.out.println(account.substring(1,6));
+////        System.out.println(account.trim());
+////        System.out.println(account.length());
+////
+////        char a = 'a';
+////        char alpha = '\u03B1';
+////
+////        System.out.println(alpha);
+////
+////        char[] witaj = {'w', 'i','t','a','j'};
+////        System.out.println(witaj);
+////        Character.isDigit(a);
+////        System.out.println(Character.isDigit(a));
+////
+////        if (liczba>liczba2 | liczba2==liczba){
+////            System.out.println("System działa");
+////        } else {
+////                System.out.println("System nie działa");
+////                if (liczba==liczba2){
+////                    System.out.println("System działa1");
+////                }
 
-       System.out.println("Hello word" + liczba);
 
-       System.out.println(liczba % liczba2);
-
-        String name = "Michał";
-        int age = 28;
-        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
-
-        String account = " MartaKubala ";
-        System.out.println(account.length());
-        System.out.println(account.indexOf('r'));
-        System.out.println(account.substring(1,6));
-        System.out.println(account.trim());
-        System.out.println(account.length());
-
-        char a = 'a';
-        char alpha = '\u03B1';
-
-        System.out.println(alpha);
-
-        char[] witaj = {'w', 'i','t','a','j'};
-        System.out.println(witaj);
-        Character.isDigit(a);
-        System.out.println(Character.isDigit(a));
-
-        if (liczba>liczba2 | liczba2==liczba){
-            System.out.println("System działa");
-        } else {
-                System.out.println("System nie działa");
-                if (liczba==liczba2){
-                    System.out.println("System działa1");
-                }
-            }
+        Scanner skaner = new Scanner(System.in);
+        String userInput = skaner.nextLine();
+        System.out.println(userInput);
 
     }
 
