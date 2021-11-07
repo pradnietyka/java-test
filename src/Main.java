037c19d6f987801612ea61383e2327196635c340
Michał <michal.lukaszewski3@gmail.com>
2021-11-07 14:16:18 +0100

    Drugi Commit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
 public class Main {
     public static void main(String[] args) {
+//
+//        int liczba = 5;
+//        int liczba2 = 2;
+//
+//        System.out.println("Hello word" + liczba);
+//
+//        System.out.println(liczba % liczba2);
+//
+//        String name = "Michał";
+//        int age = 28;
+//        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
+//
+        String account = " MartaKubala ";
 
-        int liczba = 5;
-        int liczba2 = 2;
+        System.out.println(account.length());
 
-        System.out.println("Hello word" + liczba);
+        System.out.println(account.indexOf('r'));
 
-        System.out.println(liczba % liczba2);
+        System.out.println(account.substring(1,6));
 
-        String name = "Michał";
-        int age = 28;
-        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
+        System.out.println(account.trim());
+
+        System.out.println(account.length());
+
+        char a = 'a';
+        char alpha = '\u03B1';
+
+        System.out.println(alpha);
+
+        char[] witaj = {'w', 'i','t','a','j'};
+        System.out.println(witaj);
+        Character.isDigit(a);
+        System.out.println(Character.isDigit(a));
+
+
     }
 
 }
