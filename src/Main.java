41839bdf9a093495725625c66b8d3b61f7a75d7f
Michał <michal.lukaszewski3@gmail.com>
2021-11-07 14:32:20 +0100

    Trzeci Commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-//
-//        int liczba = 5;
-//        int liczba2 = 2;
-//
-//        System.out.println("Hello word" + liczba);
-//
-//        System.out.println(liczba % liczba2);
-//
-//        String name = "Michał";
-//        int age = 28;
-//        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
-//
+
+       int liczba = 10;
+       int liczba2 = 10;
+
+       System.out.println("Hello word" + liczba);
+
+       System.out.println(liczba % liczba2);
+
+        String name = "Michał";
+        int age = 28;
+        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
+
         String account = " MartaKubala ";
-
         System.out.println(account.length());
-
         System.out.println(account.indexOf('r'));
-
         System.out.println(account.substring(1,6));
-
         System.out.println(account.trim());
-
         System.out.println(account.length());
 
         char a = 'a';
@@ -34,6 +29,14 @@ public class Main {
         Character.isDigit(a);
         System.out.println(Character.isDigit(a));
 
+        if (liczba>liczba2 | liczba2==liczba){
+            System.out.println("System działa");
+        } else {
+                System.out.println("System nie działa");
+                if (liczba==liczba2){
+                    System.out.println("System działa1");
+                }
+            }
 
     }
 
