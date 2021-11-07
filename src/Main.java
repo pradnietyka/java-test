eace12a87fb488fa245c268b20ad462d764afaaf
Michał <michal.lukaszewski3@gmail.com>
2021-11-07 13:11:04 +0100

    Pierwszy commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
-    //TODO
+    public static void main(String[] args) {
+
+        int liczba = 5;
+        int liczba2 = 2;
+
+        System.out.println("Hello word" + liczba);
+
+        System.out.println(liczba % liczba2);
+
+        String name = "Michał";
+        int age = 28;
+        System.out.println("Mam na imię " + name + " i mam " + age + " lat");
+    }
 
 }
